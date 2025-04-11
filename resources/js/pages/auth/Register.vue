8bbeaffb52ff7b5ed26f264a5d37272d995d3c18
Camerone <130955631+GabQuing@users.noreply.github.com>
2025-04-11 08:37:59 +0800

    working on the registration form inprogress

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -7,10 +7,26 @@ import { Label } from '@/components/ui/label';
 import AuthBase from '@/layouts/AuthLayout.vue';
 import { Head, useForm } from '@inertiajs/vue3';
 import { LoaderCircle } from 'lucide-vue-next';
+import { countries } from '@/data/countries';
+
+
 
 const form = useForm({
     name: '',
     email: '',
+    title: '',
+    country: '',
+    address: '',
+    city: '',
+    state: '',
+    contact_number: '',
+    web_page: '',
+    birthday: '',
+    wedding_anniv: '',
+    date_salvation: '',
+    date_baptism: '',
+    testimony: '',
+    extra_detail: '',
     password: '',
     password_confirmation: '',
 });
@@ -28,45 +44,140 @@ const submit = () => {
 
         <form @submit.prevent="submit" class="flex flex-col gap-6">
             <div class="grid gap-6">
-                <div class="grid gap-2">
-                    <Label for="name">Name</Label>
-                    <Input id="name" type="text" required autofocus :tabindex="1" autocomplete="name" v-model="form.name" placeholder="Full name" />
-                    <InputError :message="form.errors.name" />
+                <div class="flex flex-col sm:flex-row gap-2">
+                    <div class="grid gap-2 ">
+                        <Label for="email">Email address</Label>
+                        <Input id="email" type="email" required :tabindex="2"  v-model="form.email" placeholder="email@example.com" />
+                        <InputError :message="form.errors.email" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="password">Password</Label>
+                        <Input
+                            id="password"
+                            type="password"
+                            required
+                            :tabindex="3"
+                            autocomplete="new-password"
+                            v-model="form.password"
+                            placeholder="Password"
+                        />
+                        <InputError :message="form.errors.password" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="password_confirmation">Confirm password</Label>
+                        <Input
+                            id="password_confirmation"
+                            type="password"
+                            required
+                            :tabindex="4"
+                            autocomplete="new-password"
+                            v-model="form.password_confirmation"
+                            placeholder="Confirm password"
+                        />
+                        <InputError :message="form.errors.password_confirmation" />
+                    </div>
                 </div>
-
-                <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
-                    <Input id="email" type="email" required :tabindex="2" autocomplete="email" v-model="form.email" placeholder="email@example.com" />
-                    <InputError :message="form.errors.email" />
+                <div class="flex flex-col sm:flex-row gap-2">
+                    <div class="grid gap-2">
+                        <Label for="name">Full Name</Label>
+                        <Input id="name" type="text" required autofocus :tabindex="1" v-model="form.name" placeholder="Full Name" />
+                        <InputError :message="form.errors.name" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="title">Title</Label>
+                        <Input id="title" type="text" required autofocus :tabindex="1" v-model="form.title" placeholder="e.g. Mr., Mrs., Dr." />
+                        <InputError :message="form.errors.title" />
+                    </div>
                 </div>
-
+                <div class="flex flex-col sm:flex-row gap-2">
+                    <div class="grid gap-2">
+                        <Label for="birthday">Date of Birth</Label>
+                        <Input id="birthday" type="date" required autofocus :tabindex="1" v-model="form.birthday" placeholder="" />
+                        <InputError :message="form.errors.birthday" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="contact_number">Contact Number</Label>
+                        <Input id="contact_number" type="text" required autofocus :tabindex="1" v-model="form.contact_number" placeholder="Contact Number" />
+                        <InputError :message="form.errors.contact_number" />
+                    </div>
+                </div>
                 <div class="grid gap-2">
-                    <Label for="password">Password</Label>
-                    <Input
-                        id="password"
-                        type="password"
+                    <Label for="address">Complete Address</Label>
+                    <Input id="address" type="text" required autofocus :tabindex="1" v-model="form.address" placeholder="Complete Address" />
+                    <InputError :message="form.errors.address" />
+                </div>
+                <div class="flex flex-col sm:flex-row gap-2">
+                    <div class="grid gap-2">
+                        <Label for="country">Country</Label>
+                        <select
+                            id="country"
+                            v-model="form.country"
+                            class="border border-input bg-background text-sm rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
+                            required
+                        >
+                            <option value="" disabled>Select a country</option>
+                            <option v-for="country in countries" :key="country" :value="country">
+                                {{ country }}
+                            </option>
+                        </select>
+                        <InputError :message="form.errors.country" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="city">City</Label>
+                        <Input id="city" type="text" required autofocus :tabindex="1" v-model="form.city" placeholder="City" />
+                        <InputError :message="form.errors.city" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="state">State</Label>
+                        <Input id="state" type="text" required autofocus :tabindex="1" v-model="form.state" placeholder="State" />
+                        <InputError :message="form.errors.state" />
+                    </div>
+                </div>
+                <div class="grid gap-2">
+                    <Label for="web_page">Web Page</Label>
+                    <Input id="web_page" type="text" required autofocus :tabindex="1" v-model="form.web_page" placeholder="e.g. https://example.com" />
+                    <InputError :message="form.errors.web_page" />
+                </div>
+                <div class="flex flex-col sm:flex-row gap-2">
+                    <div class="grid gap-2">
+                        <Label for="wedding_anniv">Wedding Anniversary</Label>
+                        <Input id="wedding_anniv" type="date" required autofocus :tabindex="1" v-model="form.wedding_anniv" placeholder="" />
+                        <InputError :message="form.errors.wedding_anniv" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="date_salvation">Date of Salvation</Label>
+                        <Input id="date_salvation" type="date" required autofocus :tabindex="1" v-model="form.date_salvation" placeholder="" />
+                        <InputError :message="form.errors.date_salvation" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="date_baptism">Date of Baptism</Label>
+                        <Input id="date_baptism" type="date" required autofocus :tabindex="1" v-model="form.date_baptism" placeholder="" />
+                        <InputError :message="form.errors.date_baptism" />
+                    </div>
+                </div>
+                <div class="grid gap-2">
+                    <Label for="testimony">Testimony</Label>
+                    <textarea
+                        id="testimony"
+                        v-model="form.testimony"
+                        class="border border-input bg-background text-sm rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary resize-none h-32"
                         required
-                        :tabindex="3"
-                        autocomplete="new-password"
-                        v-model="form.password"
-                        placeholder="Password"
-                    />
-                    <InputError :message="form.errors.password" />
+                    >
+                    </textarea>
+                    <InputError :message="form.errors.testimony" />
+                </div>
+                <div class="grid gap-2">
+                    <Label for="extra_detail">Extra Details</Label>
+                    <textarea 
+                        id="extra_detail"
+                        v-model="form.extra_detail"
+                        class="border border-input bg-background text-sm rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary resize-none h-32"
+                        required
+                    >
+                    </textarea>
+                    <InputError :message="form.errors.extra_detail" />
                 </div>
 
-                <div class="grid gap-2">
-                    <Label for="password_confirmation">Confirm password</Label>
-                    <Input
-                        id="password_confirmation"
-                        type="password"
-                        required
-                        :tabindex="4"
-                        autocomplete="new-password"
-                        v-model="form.password_confirmation"
-                        placeholder="Confirm password"
-                    />
-                    <InputError :message="form.errors.password_confirmation" />
-                </div>
 
                 <Button type="submit" class="mt-2 w-full" tabindex="5" :disabled="form.processing">
                     <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
