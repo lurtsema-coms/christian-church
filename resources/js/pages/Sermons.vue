8dd823dd7e5f21a2297c8406dd0185222f48924a
Camerone <130955631+GabQuing@users.noreply.github.com>
2025-05-01 07:01:24 +0800

    working on the frontend sermons

--- resources/js/pages/Sermons.vue
+++ resources/js/pages/Sermons.vue
@@ -10,8 +10,14 @@
 <template>
     <Head title="Sermons" />
 
-    <div class="relative h-[39rem] bg-black">
-        
+    <div class="relative h-[39rem] mt-12">
+        <div class="flex flex-col md:flex-row justify-center items-center h-[14rem] rounded-md mb-10">
+            <img src="/img/sample_pic.jpg" alt="" class="h-full rounded-l-lg ">
+            <div class="bg-[#00457A] h-full max-w-[36rem]  p-[40px] text-white rounded-r-lg ">
+                <span class="block text-[1.5rem] font-bold text-center mb-1">Resurrection Day!</span>
+                <p class="">by Join us for a powerful Resurrection Day message delivered by Kevin M. Kelley on April 20, 2025, exploring Mark 16:1-8 and the hope found in Christ’s resurrection.</p>
+            </div>
+        </div>
     </div>
 </template>
 
