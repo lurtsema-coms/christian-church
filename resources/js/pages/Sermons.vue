5a6fb4ff37318da70e32a1edb1181ae41a6b7f19
Camerone <130955631+GabQuing@users.noreply.github.com>
2025-05-03 10:37:08 +0800

    added audio source for sermon

--- resources/js/pages/Sermons.vue
+++ resources/js/pages/Sermons.vue
@@ -11,14 +11,17 @@
     <Head title="Sermons" />
 
     <div class="relative h-[39rem] mt-12">
-        <div class="flex flex-col md:flex-row justify-center items-center px-5 md:h-[14rem] rounded-md mb-10">
+        <div class="flex flex-col md:flex-row justify-center items-center px-5 md:h-[16rem] rounded-md mb-10">
             <img src="/img/sample_pic.jpg" alt="" class="h-full w-full md:max-w-md rounded-t-lg md:rounded-t-none lg:rounded-l-lg">
             <div class="bg-[#00457A] h-full w-full md:max-w-[36rem] p-[40px] text-white rounded-b-lg md:rounded-b-none lg:rounded-r-lg">
                 <span class="block text-[1.5rem] font-bold text-center mb-1">Resurrection Day!</span>
-                <p class="">by Join us for a powerful Resurrection Day message delivered by Kevin M. Kelley on April 20, 2025, exploring Mark 16:1-8 and the hope found in Christ’s resurrection.</p>\
-                <!-- <div>
-                    add audio here
-                </div> -->
+                <p class="">by Join us for a powerful Resurrection Day message delivered by Kevin M. Kelley on April 20, 2025, exploring Mark 16:1-8 and the hope found in Christ’s resurrection.</p>
+                <div class="mt-5 rounded">
+                    <audio controls class="w-full ">
+                        <source src="/audio/resurrection-day.mp3" type="audio/mpeg">
+                        Your browser does not support the audio element.
+                    </audio>
+                </div>
             </div>
         </div>
     </div>
