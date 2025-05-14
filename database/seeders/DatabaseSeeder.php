e43e9b847b176fac740af3195f77d7830c67d5d7
Camerone <130955631+GabQuing@users.noreply.github.com>
2025-05-15 03:26:00 +0800

    create role factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use function Symfony\Component\Clock\now;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,31 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Role::factory()->createMany([
+            [
+                'title' => 'Admin',
+                'created_at' => now(), 
+            ],
+            [
+                'title' => 'User',
+                'created_at' => now(), 
+            ],
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role_id' => 1,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'user@example.com',
             'password' => bcrypt('password'),
+            'role_id' => 2,
         ]);
+
+
     }
 }
