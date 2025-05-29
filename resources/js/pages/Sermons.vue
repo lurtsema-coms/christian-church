0809aa00513479149297f08127efb0dc51409091
Camerone <130955631+GabQuing@users.noreply.github.com>
2025-05-30 07:23:05 +0800

    added background image and grey on sermons

--- resources/js/pages/Sermons.vue
+++ resources/js/pages/Sermons.vue
@@ -15,38 +15,45 @@
 <template>
     <Head title="Sermons" />
 
-    <div class="relative pb-16 pt-28">
-        <div v-for="sermon in sermons.data" :key="sermon.id" class="mt-12 flex flex-col md:flex-row justify-center items-center px-5 md:h-[16rem] rounded-md mb-10">
-            <img :src="sermon.image_url" alt="Image" class="w-full h-full rounded-t-lg md:max-w-md md:rounded-t-none lg:rounded-l-lg">
-            <div class="bg-[#00457A] h-full w-full md:max-w-[36rem] p-[40px] text-white rounded-b-lg md:rounded-b-none lg:rounded-r-lg">
-                <span class="block text-[1.5rem] font-bold text-center mb-1">{{ sermon.title }}</span>
-                <p class="">{{ sermon.description }}</p>
-                <div class="mt-5 rounded">
-                    <audio controls class="w-full ">
-                        <source :src="sermon.audio_url" type="audio/mpeg">
-                        Your browser does not support the audio element.
-                    </audio>
+    <div class="relative pb-16 pt-20">
+        <img src="/img/cardboard-texture.webp" alt="cardboard-texture-bg" class="absolute inset-0 z-0 object-cover w-full h-full mix-blend-multiply" />
+        <div class="absolute inset-0 z-0 bg-gray-100/75"></div>
+
+        <div class="relative z-10 flex flex-col px-5 mt-12">
+            <div class="z-10">
+                <div v-for="sermon in sermons.data" :key="sermon.id" class="mt-12 flex flex-col md:flex-row justify-center items-center px-5 md:h-[16rem] rounded-md mb-10">
+                    <img :src="sermon.image_url" alt="Image" class="w-full h-full rounded-t-lg md:max-w-md md:rounded-t-none lg:rounded-l-lg">
+                    <div class="bg-[#00457A] h-full w-full md:max-w-[36rem] p-[40px] text-white rounded-b-lg md:rounded-b-none lg:rounded-r-lg">
+                        <span class="block text-[1.5rem] font-bold text-center mb-1">{{ sermon.title }}</span>
+                        <p class="">{{ sermon.description }}</p>
+                        <div class="mt-5 rounded">
+                            <audio controls class="w-full ">
+                                <source :src="sermon.audio_url" type="audio/mpeg">
+                                Your browser does not support the audio element.
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+                <div class=" mt-4 flex flex-row justify-center items-center px-5 gap-20">
+                    <button
+                        class="bg-[#00457A] p-2 text-white rounded-md disabled:bg-[#00457a92]"
+                        :disabled="!sermons.prev_page_url" 
+                        @click="router.get(sermons.prev_page_url, {}, { preserveState: true })">
+                        Previous
+                    </button>
+                    
+                    <span class="text-gray-500">
+                        Page {{ sermons.current_page }} of {{ sermons.last_page }}
+                    </span>
+                    
+                    <button
+                        class="bg-[#00457A] p-2 text-white rounded-md disabled:bg-[#00457a92]"
+                        :disabled="!sermons.next_page_url" 
+                        @click="router.get(sermons.next_page_url, {}, { preserveState: true })">
+                        Next
+                    </button>
                 </div>
             </div>
-        </div>
-        <div class=" mt-4 flex flex-row justify-center items-center px-5 gap-20">
-            <button
-                class="bg-[#00457A] p-2 text-white rounded-md disabled:bg-[#00457a92]"
-                :disabled="!sermons.prev_page_url" 
-                @click="router.get(sermons.prev_page_url, {}, { preserveState: true })">
-                Previous
-            </button>
-            
-            <span class="text-gray-500">
-                Page {{ sermons.current_page }} of {{ sermons.last_page }}
-            </span>
-            
-            <button
-                class="bg-[#00457A] p-2 text-white rounded-md disabled:bg-[#00457a92]"
-                :disabled="!sermons.next_page_url" 
-                @click="router.get(sermons.next_page_url, {}, { preserveState: true })">
-                Next
-            </button>
         </div>
     </div>
 
